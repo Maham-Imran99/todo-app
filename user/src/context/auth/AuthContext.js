5e8ddf68cf8ae20ea535910a3d1f78809b9a41d7
Maham-Imran99 <maham.imran@kwanso.com>
2024-04-17 10:04:17 +0500

    protected  routes added

--- user/src/context/auth/AuthContext.js
+++ user/src/context/auth/AuthContext.js
@@ -11,7 +11,9 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   const login = (token) => {
+    console.log("here")
     localStorage.setItem('token', token);
+    console.log("token", token)
     setIsLoggedIn(true);
   };
 
@@ -28,83 +30,3 @@ export const AuthProvider = ({ children }) => {
 };
 
 export const useAuth = () => useContext(AuthContext);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { createContext, useContext, useState, useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
-
-// const AuthContext = createContext(null);
-
-// export const AuthProvider = ({ children }) => {
-//   const [auth, setAuth] = useState({
-//     token: null,
-//     isAuthenticated: false,
-//   });
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       setAuth({ token, isAuthenticated: true });
-//     }
-//   }, []);
-
-//   const login = (token) => {
-//     localStorage.setItem('token', token);
-//     setAuth({ token, isAuthenticated: true });
-//     navigate('/todos');
-//   };
-
-//   const logout = () => {
-//     localStorage.removeItem('token');
-//     setAuth({ token: null, isAuthenticated: false });
-//     navigate('/login');
-//   };
-
-//   const value = { ...auth, login, logout };
-
-//   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-// };
-
-// export const useAuth = () => useContext(AuthContext);
--- user/src/routes/routes.js
+++ user/src/routes/routes.js
@@ -3,10 +3,19 @@ import SignUpPage from '../pages/auth/SignUp';
 import HomePage from '../pages/todo/Home';
 import TodoPage from '../pages/todo/Todo';
 import { TodoProvider } from '../context/todo/TodoContext';
+import ProtectedRoute from '../components/auth/ProtectedRoute';
 
 export const routes = [
-    { path: "/", element: <HomePage /> },
-    { path: "/login", element: <LoginPage /> },
-    { path: "/signup", element: <SignUpPage /> },
-    { path: "/todos", element: <TodoProvider><TodoPage /></TodoProvider> }
+  { path: "/", element: <HomePage /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/signup", element: <SignUpPage /> },
+  {
+    path: "/todos", element: (
+      <TodoProvider>
+        <ProtectedRoute>
+          <TodoPage />
+        </ProtectedRoute>
+      </TodoProvider>
+    )
+  }
 ]
